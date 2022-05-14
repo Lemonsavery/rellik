cd80b541d9e0ca661d47de3c4cb1d1952c8b48b6
Avery Lemons <lemonsavery@gmail.com>
2022-05-13 23:18:25 -0500

    Add butterfly theme.

--- src/themes/Themes.ts
+++ src/themes/Themes.ts
@@ -1,12 +1,15 @@
 import type Theme from "./Theme";
 import theme_BASIC from "./basic";
+import theme_BUTTERFLY from "./butterflies";
 
 
 
 export const Themes: {[themeName: string]: Theme} = {
     [theme_BASIC.id]: theme_BASIC,
+    [theme_BUTTERFLY.id]: theme_BUTTERFLY,
 };
 export let menuThemeOrder = [
     theme_BASIC.id,
+    theme_BUTTERFLY.id,
 ];
 export default Themes;
